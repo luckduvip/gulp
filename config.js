5156ec083276c0a500f8b5df8db7002917c37ed0
luckduvip <luckduvip@163.com>
2016-08-08 17:14:36 +0800

    add project

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var baseUrl = {
 	sfg : {
 		mogo : "/Users/luckduvip/webapps/mogo/mogo/Public/"
+		,mogo_h5 : "/Users/luckduvip/webapps/mogo/yiyuan/web/Public/"
 	}
 }
 module.exports = baseUrl.sfg;
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,33 @@ var gulp = require('gulp')
 				]
 			}
 		}
+		,mogo_h5 : {
+			scss : {
+				input_folder : "scss/base.scss"
+				,output_folder : "css/"
+			}
+			,js : {
+				input_folder : ['js/lib/*.js','js/tools/*.js','js/htmls/*.js']
+				,output_name : "dist.js"
+				,output_folder : "js/min/"
+			}
+			,tmod : {
+				baseUrl : "/Users/luckduvip/webapps/mogo/mogo/Public/tmod/"
+				,input_folder : "tpl/**/*.html"
+				,output_folder : "js/min"
+			}
+			,watch : {
+				watchList : ["tmod/tpl/**/*.*","tmod/myHelp.js","scss/","scss/**/*.*","js/lib/*.*","js/tools/**/*.js","js/htmls/**/*.*"]
+				,callbackList : [
+					{ key : /tmod\/myHelp\.js/i , callback : "project" , params : "mogo" }
+					,{ key : /tmod\/tpl/i , callback : "project" , params : "mogo" }
+					,{ key : /scss\/[\w_]+\.scss/i , callback : "project" , params : "mogo" }
+					,{ key : /js\/lib\/[\w_]+\.js/i , callback : "project" , params : "mogo" }
+					,{ key : /js\/htmls\/[\w_]+\.js/i , callback : "project" , params : "mogo" }
+					,{ key : /js\/tools\/[\w_]+\.js/i , callback : "project" , params : "mogo" }
+				]
+			}
+		}
 	}
 gulp.task('project_tmod',function(url,config){
 	return gulp.src(url + "tmod/" +config.input_folder)
